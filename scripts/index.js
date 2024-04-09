972a069d5a2d1ca6fb2de5039628220b4b9f5c6e
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-04-09 11:03:54 +0300

    JS was added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,74 @@
-// @todo: Темплейт карточки
+const list = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function addCard (name, link, deleteCard) {
+  const listItem = document.querySelector('#card-template');
+  const listItemCopy = listItem.content.cloneNode(true);
+  const deleteButton = listItemCopy.querySelector('.card__delete-button');
+  listItemCopy.querySelector('.card__title').textContent = name;
+  listItemCopy.querySelector('.card__image').src = link;
+  deleteButton.addEventListener('click', (event) => deleteCard(event));
+  return listItemCopy
+}
 
-// @todo: Функция создания карточки
+function deleteCard () {
+  const card = event.target.closest('.card');
+  card.remove();
+}
 
-// @todo: Функция удаления карточки
+initialCards.forEach(function (item) {
+  list.append(addCard(item.name, item.link, deleteCard));
+});
 
-// @todo: Вывести карточки на страницу
+// ---------------------------------------------------------------------------------------------
+
+// // @todo: Темплейт карточки
+
+// const listItem = document.querySelector('#card-template').content;
+
+// // @todo: DOM узлы
+
+// const list = document.querySelector('.places__list');
+
+// // @todo: Функция создания карточки
+
+// function addCard (name, link, deleteCard) {
+//   const listItemCopy = listItem.querySelector('.card').cloneNode(true);
+//   const deleteButton = listItemCopy.querySelector('.card__delete-button');
+//   listItemCopy.querySelector('.card__title').textContent = name;
+//   listItemCopy.querySelector('.card__image').src = link;
+//   deleteButton.addEventListener('click',(event) => deleteCard(event));
+//   list.append(listItemCopy);
+// }
+
+// // @todo: Функция удаления карточки
+
+// function deleteCard () {
+//   const card = event.target.closest('.card');
+//   card.remove();
+// }
+
+// // @todo: Вывести карточки на страницу
+
+// initialCards.forEach(function (item) {
+//   addCard(item.name, item.link, deleteCard);
+// });
+
+// ---------------------------------------------------------------------------------------------
+
+// function addCard (name, link) {
+//   const listItem = document.querySelector('#card-template');
+//   const listItemCopy = listItem.content.cloneNode(true);
+//   const list = document.querySelector('.places__list');
+//   const deleteButton = listItemCopy.querySelector('.card__delete-button');
+//   listItemCopy.querySelector('.card__title').textContent = name;
+//   listItemCopy.querySelector('.card__image').src = link;
+//   deleteButton.addEventListener('click', () => {
+//     const card = deleteButton.closest('.card');
+//     card.remove();
+//   });
+//   list.append(listItemCopy);
+// }
+
+// initialCards.forEach(function (item) {
+//   addCard(item.name, item.link);
+// });
